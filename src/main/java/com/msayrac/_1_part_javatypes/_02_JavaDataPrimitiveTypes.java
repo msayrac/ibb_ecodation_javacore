d0b76044d3d84b519b8151308cc33acb72828a92
msayrac <msayracode@gmail.com>
2025-02-26 09:15:17 +0300

    Java Primitie Types studied

--- src/main/java/com/msayrac/_1_part_javatypes/_02_JavaDataPrimitiveTypes.java
+++ src/main/java/com/msayrac/_1_part_javatypes/_02_JavaDataPrimitiveTypes.java
@@ -4,6 +4,7 @@ public class _02_JavaDataPrimitiveTypes {
     public static void main(String[] args) {
 
        /*
+
         primitive type 8 adet --> byte short int long float double boolean char. null değer alamaz
         Tam sayılar 4 adet
 
