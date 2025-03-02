441d8f1597b30e61f24ae6a921e935c3316bf1fe
msayrac <msayracode@gmail.com>
2025-03-02 16:02:17 +0300

    conditional is studied

---
+++ src/main/java/com/msayrac/_1_part_javatypes/_9_Conditional.java
@@ -0,0 +1,19 @@
+package com.msayrac._1_part_javatypes;
+
+public class _9_Conditional {
+
+    public static void main(String[] args) {
+
+        int number   =0;
+
+        if (number>0){
+            System.out.println("Number > 0 +");
+        }
+        else if (number<0){
+            System.out.println("Number < 0 -   ");
+        }
+        else{
+            System.out.println("Number  = 0");
+        }
+    }
+}
