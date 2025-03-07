cd61c3ee518c40cc805398fa4cec9516f4a2d5ec
msayrac <msayracode@gmail.com>
2025-03-07 09:46:25 +0300

    Casting are investigated

---
+++ src/main/java/com/msayrac/_1_part_javatypes/_14_Casting.java
@@ -0,0 +1,29 @@
+package com.msayrac._1_part_javatypes;
+
+public class _14_Casting {
+
+    public static void main(String[] args) {
+
+        //    casting bir verinin baska veriye donusmesine casting denir
+
+        int cast1int=100;
+        double cast1double = cast1int;
+        System.out.println(cast1double);
+
+        int cast2int =12152154;
+        byte cast2Byte = (byte)cast2int;
+        System.out.println(cast2Byte);
+
+        char cast3 = 'A';
+        int asciiChar = cast3;
+        System.out.println(asciiChar);
+
+        int cast4 =38;
+        char asci4 = (char) cast4;
+        System.out.println(asci4);
+
+
+    }
+
+
+}
