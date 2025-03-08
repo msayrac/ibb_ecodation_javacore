ec1484977868ab363406492cbaeff261f522bd18
msayrac <msayracode@gmail.com>
2025-03-08 16:42:55 +0300

    dateFormat

--- src/main/java/com/msayrac/_1_part_javatypes/_14_Exception.java
+++ src/main/java/com/msayrac/_1_part_javatypes/_14_Exception.java
@@ -1,20 +1,34 @@
 package com.msayrac._1_part_javatypes;
 
+import java.util.Date;
+
 public class _14_Exception {
 
-    public static void main(String[] args) {
-        int num1 = 25;
-        int num2 = 0;
+    public static void dateMethod() {
 
-        try {
-            int result = num1 / num2;
-            System.out.println(result);
-        } catch (ArithmeticException ai) {
-//            ai.printStackTrace();
-        }
-        finally {
-            System.out.println("son  satır");
-        }
+        Date date = new Date();
+        System.out.println("1 Ocak 1970 den şu ana kadar geçen mili saniye : " + date.getTime());
+        System.out.println("Day : " + date.getDate());
+        System.out.println("Month : " + date.getMonth());
+        System.out.println("Year : " + date.getYear());
+        System.out.println("Hours : " + date.getHours());
+    }
+
+    public static String nowFormat1() {
+        Date date = new Date();
+        String specialForm = "zaman : ".concat(String.valueOf(date.getHours())).concat(String.valueOf(date.getMinutes())).concat(String.valueOf(date.getSeconds())).toString();
+
+        return specialForm;
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+//        dateMethod();
+        System.out.println(nowFormat1());
+
 
     }
 
