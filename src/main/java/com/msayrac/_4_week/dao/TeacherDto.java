c243a319dd97df5d54fe80cfd2789c185a2705b2
msayrac <msayracode@gmail.com>
2025-04-10 14:09:25 +0300

    ENUM

--- src/main/java/com/msayrac/_4_week/dao/TeacherDto.java
+++ src/main/java/com/msayrac/_4_week/dao/TeacherDto.java
@@ -1,17 +1,27 @@
 package com.msayrac._4_week.dao;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+
+enum ETeacherSubject {
+    MATHEMATICS,
+    CHEMISTRY,
+    BIOLOGY,
+    HISTORY,
+    COMPUTER_SCIENCE
+}
 
 public record TeacherDto(
-        PersonDto personDto,
-        String subject,
+        Integer id,
+        String name,
+        String surname,
+        LocalDate birthdate,
+        ETeacherSubject subject,
         int yearsOfExperience,
         boolean isTenured,
         double salary
 
-
 ) implements Serializable {
-
 
 
 }
